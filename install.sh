a83a0aace18d7841a98992b7d20648f62d3f143b
ADMinOKMisha <103426989+ADMinOKMisha@users.noreply.github.com>
2022-04-11 14:59:38 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+pkg upgrade && pkg update && pkg install python && pkg install python2 && pkg install python3 && pkg install git && pip install requests && pip install clorama && pip install telethon
