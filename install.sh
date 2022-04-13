b7f7465ef95ebf0f34621b22ca555a45d7804bcd
ADMinOKMisha <103426989+ADMinOKMisha@users.noreply.github.com>
2022-04-13 04:01:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkg upgrade && pkg update && pkg install python && pkg install python2 && pkg install python3 && pip install requests && pip install colorama && pip install telethon && > .api_id && > .api_id && python tg.py
+pkg upgrade && pkg update && pkg install python && pkg install python2 && pkg install python3 && pip install requests && pip install colorama && pip install telethon && > .api_id && > .api_id && bash .install.sh
